9e8087a793dfc8d898033617773263e47c8c85ba
Niclas Titius <niclas.titius@nicti.de>
2025-10-03 21:00:19 +0200

    Fix lack of function end

--- util/subgroup.js
+++ util/subgroup.js
@@ -1,7 +1,7 @@
 export async function handleGroupName(db, interaction) {
     const sub = await db.subsCollection.findOne({channelId: interaction.channel.id});
     const groupName = interaction.options.getString('group_name');
-    if (!sub || !sub.subgroups) interaction.respond([])
+    if (!sub || !sub.subgroups) return interaction.respond([]);
     interaction.respond(Object.keys(sub.subgroups)
         .filter(g => g.toLowerCase().includes(groupName.toLowerCase()))
         .map(g => ({name: g, value: g}))
