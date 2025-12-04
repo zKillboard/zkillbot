54ba6b41df895a1f489c1d2b966596dca614b06e
cvweiss <cvweiss@gmail.com>
2025-12-04 13:28:48 -0500

    includes new fw labels

--- src/util/constants.js
+++ src/util/constants.js
@@ -10,7 +10,9 @@ export const HEADERS = {
 export const SUGGESTION_LABEL_FILTERS = [
 	"#:1", "#:5+", "#:10+", "#:25+", "#:50+", "#:100+", 
 	"awox", "bigisk", "capital",
-	"concord", "insaneisk",
+	"concord",
+	"fw:amarr", "fw:caldari", "fw:gallente", "fw:minmatar", "fw:amamin", "fw:calgal",
+	"insaneisk",
 	"loc:abyssal", "loc:drifter", "loc:highsec", "loc:lowsec", "loc:nullsec", "loc:w-space",
 	"npc", "pvp", "solo",
 	"tz:au", "tz:eu", "tz:ru", "tz:use", "tz:usw"
@@ -20,7 +22,9 @@ export const LABEL_FILTERS = [
 	"#:1", "#:2+", "#:5+", "#:10+", "#:25+", "#:50+", "#:100+", "#:1000+",
 	"atShip", "awox", "bigisk", "capital",
 	"cat:11", "cat:18", "cat:22", "cat:23", "cat:350001", "cat:40", "cat:46", "cat:6", "cat:65", "cat:87",
-	"concord", "extremeisk", "ganked", "insaneisk",
+	"concord", "extremeisk",
+	"fw:amarr", "fw:caldari", "fw:gallente", "fw:minmatar", "fw:amamin", "fw:calgal",
+	"ganked", "insaneisk",
 	"isk:100b+", "isk:10b+", "isk:1b+", "isk:1t+", "isk:5b+", "isk:1t+",
 	"loc:abyssal", "loc:drifter", "loc:highsec", "loc:lowsec", "loc:nullsec", "loc:w-space",
 	"npc", "padding", "pvp", "solo",
