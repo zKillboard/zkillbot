da26c93277af01a24ba18ec0a36749aa05d1b798
cvweiss <cvweiss@gmail.com>
2025-12-15 16:26:10 -0500

    put match types in their own block

--- src/services/poll-redisq.js
+++ src/services/poll-redisq.js
@@ -50,21 +50,23 @@ export async function pollRedisQ(db, REDISQ_URL) {
 			}			
 
 			// Victims
-			if (victimEntities.length > 0) {
-				const matchingSubs = await db.subsCollection
-					.find({ entityIds: { $in: victimEntities } })
-					.toArray();
-				for (const match of matchingSubs) {
-					let entityIds = match.entityIds.filter(Boolean) || [];
-					if (entityIds.length === 0) continue;
-					// lets validate that victimEntities is actually in entityIds (mistrusting mongo $in)
-					const found = victimEntities.find(e => entityIds.includes(e));
-					if (!found) continue;
+			{
+				if (victimEntities.length > 0) {
+					const matchingSubs = await db.subsCollection
+						.find({ entityIds: { $in: victimEntities } })
+						.toArray();
+					for (const match of matchingSubs) {
+						let entityIds = match.entityIds.filter(Boolean) || [];
+						if (entityIds.length === 0) continue;
+						// lets validate that victimEntities is actually in entityIds (mistrusting mongo $in)
+						const found = victimEntities.find(e => entityIds.includes(e));
+						if (!found) continue;
 
-					let colorCode = 15548997; // red
-					const channelId = match.channelId;
-					const guildId = match.guildId;
-					discord_posts_queue.push({ db, match, guildId, channelId, killmail, zkb, colorCode, matchType: 'victim' });
+						let colorCode = 15548997; // red
+						const channelId = match.channelId;
+						const guildId = match.guildId;
+						discord_posts_queue.push({ db, match, guildId, channelId, killmail, zkb, colorCode, matchType: 'victim' });
+					}
 				}
 			}
 
@@ -92,65 +94,71 @@ export async function pollRedisQ(db, REDISQ_URL) {
 			attackerEntities.push(constellation.region_id);
 
 			// Attackers
-			if (attackerEntities.length > 0) {
-				const matchingSubs = await db.subsCollection
-					.find({ entityIds: { $in: attackerEntities, $exists: true, $ne: [] } })
-					.toArray();
-				for (const match of matchingSubs) {
-					if (!match.entityIds) continue; // wtf, should never happen, but it does
-					let entityIds = match.entityIds.filter(Boolean) || [];
-					if (entityIds.length === 0) continue;
-					// lets validate that attackerEntities is actually in entityIds (mistrusting mongo $in)
-					const found = attackerEntities.find(e => entityIds.includes(e));
-					if (!found) continue;
+			{
+				if (attackerEntities.length > 0) {
+					const matchingSubs = await db.subsCollection
+						.find({ entityIds: { $in: attackerEntities, $exists: true, $ne: [] } })
+						.toArray();
+					for (const match of matchingSubs) {
+						if (!match.entityIds) continue; // wtf, should never happen, but it does
+						let entityIds = match.entityIds.filter(Boolean) || [];
+						if (entityIds.length === 0) continue;
+						// lets validate that attackerEntities is actually in entityIds (mistrusting mongo $in)
+						const found = attackerEntities.find(e => entityIds.includes(e));
+						if (!found) continue;
 
-					let colorCode = 5763719; // green
-					const channelId = match.channelId;
-					const guildId = match.guildId;
-					discord_posts_queue.push({ db, match, guildId, channelId, killmail, zkb, colorCode, matchType: 'attacker' });
+						let colorCode = 5763719; // green
+						const channelId = match.channelId;
+						const guildId = match.guildId;
+						discord_posts_queue.push({ db, match, guildId, channelId, killmail, zkb, colorCode, matchType: 'attacker' });
+					}
 				}
 			}
 
 			// ISK
-			if (zkb.totalValue >= 100000000) { // 100m minimum
-				const matchingSubs = await db.subsCollection
-					.find({
-						iskValue: { $gte: 100000000, $lte: zkb.totalValue }
-					})
-					.toArray();
-				for (const match of matchingSubs) {
-					// lets validate that zkb.totalValue is actually in the range
-					if (!match.iskValue) continue; // wtf, should never happen, but it does
-					if (isNaN(match.iskValue)) continue;
-					if (zkb.totalValue < 100000000) continue;
-					if (zkb.totalValue < match.iskValue) continue;
+			{
+				if (zkb.totalValue >= 100000000) { // 100m minimum
+					const matchingSubs = await db.subsCollection
+						.find({
+							iskValue: { $gte: 100000000, $lte: zkb.totalValue }
+						})
+						.toArray();
+					for (const match of matchingSubs) {
+						// lets validate that zkb.totalValue is actually in the range
+						if (!match.iskValue) continue; // wtf, should never happen, but it does
+						if (isNaN(match.iskValue)) continue;
+						if (zkb.totalValue < 100000000) continue;
+						if (zkb.totalValue < match.iskValue) continue;
 
-					// if we got here, we have a match
+						// if we got here, we have a match
 
-					let colorCode = 12092939; // gold
-					const channelId = match.channelId;
-					const guildId = match.guildId;
-					discord_posts_queue.push({ db, match, guildId, channelId, killmail, zkb, colorCode, matchType: 'isk' });
+						let colorCode = 12092939; // gold
+						const channelId = match.channelId;
+						const guildId = match.guildId;
+						discord_posts_queue.push({ db, match, guildId, channelId, killmail, zkb, colorCode, matchType: 'isk' });
+					}
 				}
 			}
 
 			// Labels
-			const labels = zkb.labels.filter(Boolean);
-			if (labels.length > 0) { // length of 0 shouldn't happen, but just in case
-				const matchingSubs = await db.subsCollection
-					.find({ labels: { $in: labels, $exists: true, $ne: [] } })
-					.toArray();
-				for (const match of matchingSubs) {
-					let labels = match.labels.filter(Boolean) || [];
-					if (labels.length === 0) continue;
-					// lets validate that labels is actually in entityIds (mistrusting mongo $in)
-					const found = labels.find(e => zkb.labels.includes(e));
-					if (!found) continue;
+			{
+				const labels = zkb.labels.filter(Boolean);
+				if (labels.length > 0) { // length of 0 shouldn't happen, but just in case
+					const matchingSubs = await db.subsCollection
+						.find({ labels: { $in: labels, $exists: true, $ne: [] } })
+						.toArray();
+					for (const match of matchingSubs) {
+						let labels = match.labels.filter(Boolean) || [];
+						if (labels.length === 0) continue;
+						// lets validate that labels is actually in entityIds (mistrusting mongo $in)
+						const found = labels.find(e => zkb.labels.includes(e));
+						if (!found) continue;
 
-					let colorCode = 3569059; // dark blue
-					const channelId = match.channelId;
-					const guildId = match.guildId;
-					discord_posts_queue.push({ db, match, guildId, channelId, killmail, zkb, colorCode, matchType: 'label' });
+						let colorCode = 3569059; // dark blue
+						const channelId = match.channelId;
+						const guildId = match.guildId;
+						discord_posts_queue.push({ db, match, guildId, channelId, killmail, zkb, colorCode, matchType: 'label' });
+					}
 				}
 			}
 
